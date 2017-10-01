75e29ae1fab862de90d4ede32c2cc4bca30a8d77
alswnsWkd21 <djflqjfl0209@gmail.com>
2017-10-02 01:45:39 +0900

    Gps location init complete

--- app/src/main/java/smc/minjoon/accompanying/DaummapActivity.java
+++ app/src/main/java/smc/minjoon/accompanying/DaummapActivity.java
@@ -18,13 +18,16 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 public class DaummapActivity extends AppCompatActivity {
     private String TAG = "permissionstatus";
     String a ;
     LocationManager lm;
+    Location userlocation;
     TextView tv01;
+    MapView mapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,11 +40,14 @@ public class DaummapActivity extends AppCompatActivity {
         if (isGrantStorage) {
 // 일반처리.   // MapView 객체생성 및 API Key 설정
             Toast.makeText(this, "권한을 허락하셨습니다.", Toast.LENGTH_SHORT).show();
-            MapView mapView = new MapView(DaummapActivity.this);
+             mapView = new MapView(DaummapActivity.this);
             mapView.setDaumMapApiKey("fddf269318894c9791ec626cc3d6c8f9");
             RelativeLayout mapViewContainer = (RelativeLayout) findViewById(R.id.map_view);
             mapViewContainer.addView(mapView);
 
+            LocationManager lm = (LocationManager)getSystemService(Context. LOCATION_SERVICE);
+            lm.removeUpdates( mLocationListener );    // Stop the update if it is in progress.
+            lm.requestLocationUpdates( LocationManager.GPS_PROVIDER , 0, 0, mLocationListener );
 
         }
 
@@ -61,18 +67,10 @@ public class DaummapActivity extends AppCompatActivity {
                 lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,1,mLocationListener);
                 return true;
                 //만약 두개의 퍼미션이 전부 허락되어있다면 true반환
-            } else if(checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                Log.v(TAG, "Permission is revoked");
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-                return false;
-                //만약 Fine퍼미션이 안되어 있다면 요청
-            } else if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
-                return false;
-                //만약 COARSE퍼미션이 안되어 있다면 요청
+
             } else {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
+
                 //만약 두개의 퍼미션이 전부 안되어 있다면 요청
                 return false;
             }
@@ -95,27 +93,13 @@ public class DaummapActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (Build.VERSION.SDK_INT >= 23) {
-            if(requestCode == 1){
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Log.v(TAG, "Permission: " + permissions[0] + "was " + grantResults[0]);
                     finish();
                     startActivity(new Intent(this, DaummapActivity.class));
-
                 } else {
-
                     Toast.makeText(this, "access fine권한을 거절하셨습니다. 도움길서비스에 제약이 있습니다.", Toast.LENGTH_LONG).show();
                 }
-            }else{
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.v(TAG, "Permission: " + permissions[0] + "was " + grantResults[0]);
-                    finish();
-                    startActivity(new Intent(this, DaummapActivity.class));
-
-                } else {
-
-                    Toast.makeText(this, "access coarse권한을 거절하셨습니다. 도움길서비스에 제약이 있습니다.", Toast.LENGTH_LONG).show();
-                }
-            }
         }
 
     }
@@ -126,17 +110,30 @@ public class DaummapActivity extends AppCompatActivity {
     }
 
     private final LocationListener mLocationListener = new LocationListener() {
+        double longitude;
+        double latitude;
+        float accuracy;
         @Override
         public void onLocationChanged(Location location) {
             if(location.getProvider().equals(LocationManager.GPS_PROVIDER)){
-                double longitude = location.getLongitude();
-                double latitude = location.getLatitude();
-                float accuracy = location.getAccuracy();
-                a = Double.toString(longitude);
-                tv01.setText(a);
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+                accuracy = location.getAccuracy();
+                userlocation = location;
+                mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(latitude, longitude), 2,true);
+                tv01.setText(Double.toString(longitude)+"ㅇㅇ"+Double.toString(latitude));
+
+                lm.removeUpdates( this );
+
             }
             else{
-                
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+                accuracy = location.getAccuracy();
+                mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(latitude, longitude), 2,true);
+                tv01.setText(Double.toString(longitude)+"ㅇㅇ"+Double.toString(latitude));
+
+                lm.removeUpdates( this );
             }
         }
 
