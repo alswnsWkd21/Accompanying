c6e0246f1e3a70574347a5e91865c2c72869354a
alswnsWkd21 <djflqjfl0209@gmail.com>
2017-10-03 23:24:07 +0900

    add boot completed

---
+++ app/src/main/java/smc/minjoon/accompanying/BootReceiver.java
@@ -0,0 +1,23 @@
+package smc.minjoon.accompanying;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by skaqn on 2017-10-03.
+ */
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            Intent i = new Intent(context, ScreenService.class);
+            context.startService(i);
+        }
+    }
+}
+
+
