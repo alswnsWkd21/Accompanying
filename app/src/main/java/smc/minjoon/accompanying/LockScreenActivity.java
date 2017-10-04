8ea364eb1a983496db3138cdf3f53697ecf8432b
alswnsWkd21 <djflqjfl0209@gmail.com>
2017-10-04 19:35:13 +0900

    add longclick

--- app/src/main/java/smc/minjoon/accompanying/LockScreenActivity.java
+++ app/src/main/java/smc/minjoon/accompanying/LockScreenActivity.java
@@ -2,7 +2,11 @@ package smc.minjoon.accompanying;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LockScreenActivity extends AppCompatActivity {
 
@@ -10,7 +14,35 @@ public class LockScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lock_screen);
-
+        Button btnsos01 = (Button) findViewById(R.id.btnsos01);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+
+
+        btnsos01.setOnTouchListener(new View.OnTouchListener() {
+            long time_down;
+            long time_up;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                switch (action){
+                    case MotionEvent.ACTION_DOWN:
+                        time_down =  System.currentTimeMillis();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        time_up = System.currentTimeMillis();
+                        if(time_up - time_down > 5000){
+                            Toast.makeText(LockScreenActivity.this, "5초를 초과했어 성공", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(LockScreenActivity.this, "일반 클릭", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                }
+
+                return true;
+            }
+        });
+
     }
 }
